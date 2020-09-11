b97b2b99dd02574575141753a42e55815491aa70
numin123 <numin123@gmail.com>
2020-09-11 19:20:25 +0700

    first commit

---
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -0,0 +1,21 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ATMConfig.class);
+
+        ATM atm = context.getBean(ATM.class);
+        Bank bank = context.getBean(Bank.class);
+//        DataSourceDB dataSourceDB = new DataSourceDB();
+//        DataSourceFile dataSourceFile = new DataSourceFile("customers.txt");
+//        Bank bank = new Bank("My Bank", dataSourceFile);
+//        ATM atm = new ATM(bank);
+        AtmUI atmUI = new AtmUI(atm);
+        atmUI.run();
+    }
+}
